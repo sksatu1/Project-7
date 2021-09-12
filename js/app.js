d0fb144ff96cb7e4c3b42262a3ee7681e3f7e54b
sksatu1 <satueee18@gmail.com>
2021-09-13 01:52:12 +0600

    3rd

--- js/app.js
+++ js/app.js
@@ -77,5 +77,5 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
