765408b5b39971a318b96944f7ecbbd22ff2b9f3
sksatu1 <satueee18@gmail.com>
2021-09-13 03:12:06 +0600

    4th

--- js/app.js
+++ js/app.js
@@ -21,7 +21,12 @@ const showProducts = (products) => {
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" class="btn btn-danger">Details</button>
+      <br>
+      <br>
+      <p>avg rating : ${product.rating.rate}</p>
+      <p>Total reviews : ${product.rating.count}</p>
+      </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
